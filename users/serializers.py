375a98b8229eca5f3b6a6d215fcbea5faa56ac1a
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-02-12 04:52:53 +0100

    Validate user profile

--- users/serializers.py
+++ users/serializers.py
@@ -1,18 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.models import User
+from .models import Profile
+
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
     email = serializers.EmailField(required=True)
+    phone = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True)
+    
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"] #add firstname lastname phonenumber later
+        fields = ["username", "email", "phone", "password"]
     
     def validate_email(self, value):
         if User.objects.filter(email__iexact=value).exists():
             raise serializers.ValidationError("Email already exists.")
+        return value
+    
+    def validate_phone(self, value):
+        if not value.isdigit():
+            raise serializers.ValidationError("Phone must contain digits only.")
+        if len(value) <= 10:
+            raise serializers.ValidationError("Phone number too short.")
+        if Profile.objects.filter(phone=value).exists():
+            raise serializers.ValidationError("Phone number already exists.")
         return value
     
     def validate_password(self, value):
@@ -20,9 +33,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         return value
     
     def create(self, validated_data):
+        phone = validated_data.pop("phone")
+
+
+        if Profile.objects.filter(phone=phone).exists():
+            raise serializers.ValidationError({"phone": "Phone number already exists."})
+
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
+
+        Profile.objects.create(user=user, phone=phone)
+
         return user

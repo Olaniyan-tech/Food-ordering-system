9df4d41faf9fdde2055822c6422fc01b2e69e618
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-03-14 20:00:26 +0100

    Integrate payment service

---
+++ food/services/payment_service.py
@@ -0,0 +1,55 @@
+import requests
+import uuid
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+
+def initialize_payment(order):
+    if order.status != "CONFIRMED":
+        raise ValidationError("Only confirmed orders can be paid for")
+    
+    if order.total <= 0:
+        raise ValidationError("Order total must be greater than 0")
+
+    reference = f"ORDER-{order.id}-{uuid.uuid4().hex[:8].upper()}"
+
+    headers ={
+        "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "email": order.user.email,
+        "amount": int(order.total * 100),
+        "reference": reference,
+        "metadata": {
+            "order_id": order.id,
+            "user_id": order.user.id,
+            "username": order.user.username,
+        }
+    }
+
+    response = requests.post(
+        f"{settings.PAYSTACK_BASE_URL}/transaction/initialize",
+        json=payload,
+        headers=headers
+    )
+
+    if response.status_code != 200:
+        raise ValidationError("Payment initialization failed. Try again.")
+    
+    data = response.json()
+
+    if not data.get("status"):
+        raise ValidationError(data.get("message", "Payment initialization failed"))
+    
+    order.payment_reference = reference
+    order.payment_status = "PENDING"
+    order.save(updated_fields=["payment_reference", "payment_status", "updated"])
+
+    return data["data"]["authorization_url"], reference
+
+def verify_payment(reference):
+    headers ={
+        "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}"
+    }

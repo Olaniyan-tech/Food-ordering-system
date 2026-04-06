4774e2a39c334238dd132cfeab4777fead1be12b
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-04-06 23:34:57 +0100

    refactor(permissions): remove unused permissions, keep only active ones

--- food/permissions.py
+++ food/permissions.py
@@ -1,26 +1,14 @@
 from rest_framework.permissions import BasePermission
 
-class IsStaffOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method == "GET":
-            return request.user.is_authenticated
-        return request.user.is_staff
-
-class IsOrderOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
 
 class IsStaff(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_staff
-    
-class IsVendor(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            hasattr(request.user, "vendor")
-        )
 
+class IsOrderOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+    
 class IsApprovedVendor(BasePermission):
     def has_permission(self, request, view):
         return (
@@ -36,10 +24,3 @@ class IsVendorOwner(BasePermission):
             return False
         return obj.vendor == request.user.vendor
 
-class IsStaffOrVendorOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_staff:
-            return True
-        if not hasattr(request.user, "vendor"):
-            return False
-        return obj.vendor == request.user.vendor

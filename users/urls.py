d2614483d7e0975a3c7562c9a3920fa57f3614d4
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-04-08 15:39:56 +0100

    Add user profile url

--- users/urls.py
+++ users/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('login/', CookieTokenObtainPairView.as_view(), name="login"),
     path('token/refresh/', CookieTokenRefreshView.as_view(), name="token_refresh"),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', UserProfileView.as_view(), name="profile")
+    path('my_profile/', UserProfileView.as_view(), name="profile")
 ]
